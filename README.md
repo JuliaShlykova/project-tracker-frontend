74dcf2e5ceb0410a4808474ce4b697d6ee32877e
Julia Shlykova <julia.shlykova17@gmail.com>
2024-07-04 16:10:15 +0700

    Compress images for landing

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Issue tracker (frontend)
 ## Assets
 - icons were obtained from [svgrepo](https://www.svgrepo.com/);
+- images on langin page were obtained from [pixabay](https://pixabay.com/);
--- src/routes/Landing.jsx
+++ src/routes/Landing.jsx
@@ -1,17 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
-import background from '../assets/landing/background.png';
+import background from '../assets/landing/background.jpg';
 import mountain1 from '../assets/landing/mountain1.png';
 import mountain2 from '../assets/landing/mountain2.png';
 import trail from '../assets/landing/trail.png';
 import shadow from '../assets/landing/shadow.png';
 import sunnyRays from '../assets/landing/sunny-rays.png';
+import gsap from 'gsap';
+
+let timeline = gsap.timeline();
 
 const Landing = () => {
   const [mousePos, setMousePos] = useState({ x: 0, y: 0});
 
   useEffect(() => {
     const handleMouseMove = (e) => {
+      // if (timeline.isActive) return;
       setMousePos({ x: e.clientX - window.innerWidth / 2, y: e.clientY - window.innerHeight / 2 });
     };
 
@@ -21,6 +25,21 @@ const Landing = () => {
       window.removeEventListener('mouseMove', handleMouseMove);
     }
   }, [])
+
+  // useLayoutEffect(() => {
+    // let bgHeight = 2000/3200*1800;
+
+    // timeline.from(
+    //   '.bg-img', 
+    //   {
+    //     top: `${bgHeight / 2 - 600}px`,
+    //     duration: 3.5,
+    //     ease: "power3.out"
+    //   },
+    //   "1"
+    // )
+  // },[])
+
 
   return (
     <div id='landing-page-container'>
@@ -36,7 +55,7 @@ const Landing = () => {
         className='parallax bg-img' 
         alt="" 
         style={{
-          transform: `translateX(calc(-50% + ${-mousePos.x * 0.1}px)) translateY(calc(-50% + ${-mousePos.y * 0.1}px))`
+          transform: `translateX(calc(-50% + ${-mousePos.x * 0.01}px)) translateY(calc(-50% + ${-mousePos.y * 0.01}px))`
         }} 
       />
       <img 
@@ -44,13 +63,13 @@ const Landing = () => {
         className='parallax mountain1' 
         alt="" 
         style={{
-          transform: `translateX(calc(${mousePos.x<0?(-mousePos.x * 0.2):0}px)) translateY(calc(${-mousePos.y * 0.2}px))`
+          transform: `translateX(calc(${mousePos.x<0?(-mousePos.x * 0.02):0}px)) translateY(calc(${-mousePos.y * 0.02}px))`
         }} 
       />
       <div 
         className="text parallax"
         style={{
-          transform: `translateX(calc(-50% + ${-mousePos.x * 0.2}px)) translateY(calc(-50% + ${-mousePos.y * 0.2}px))`
+          transform: `translateX(calc(-50% + ${-mousePos.x * 0.03}px)) translateY(calc(-50% + ${-mousePos.y * 0.03}px))`
         }} 
       >
         <h1>Project Tracker</h1>
@@ -60,7 +79,7 @@ const Landing = () => {
         className='parallax mountain2' 
         alt="" 
         style={{
-          transform: `translateX(calc(${mousePos.x>0?(-mousePos.x * 0.3):0}px)) translateY(calc(${-mousePos.y * 0.3}px))`
+          transform: `translateX(calc(${mousePos.x>0?(-mousePos.x * 0.04):0}px)) translateY(calc(${-mousePos.y * 0.04}px))`
         }} 
       />
       <img 
@@ -68,7 +87,7 @@ const Landing = () => {
         className='trail parallax' 
         alt="" 
         style={{
-          transform: `translateX(${mousePos.x<0?(-mousePos.x * 0.4):0}px)`
+          transform: `translateX(${mousePos.x<0?(-mousePos.x * 0.05):0}px)`
         }} 
       />  
       <img 
