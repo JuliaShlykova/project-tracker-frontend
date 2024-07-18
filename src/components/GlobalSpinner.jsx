813318752400b9b65d95549e3213e6f7b433a726
Julia Shlykova <julia.shlykova17@gmail.com>
2024-07-18 14:06:09 +0700

    Add static page to show until react renders

--- src/components/GlobalSpinner.jsx
+++ src/components/GlobalSpinner.jsx
@@ -4,13 +4,9 @@ import { Modal, Spinner } from 'react-bootstrap'
 const GlobalSpinner = () => {
   return (
     <div className='global-spinner-wrapper w-100 h-100 fixed-top'>
-    {/* <Modal fullscreen show>
-    <Modal.Body className='bg-gray d-flex align-items-center justify-content-center'> */}
     <Spinner animation="border" role="status">
       <span className="visually-hidden">Loading...</span>
     </Spinner>
-    {/* </Modal.Body>
-    </Modal> */}
     </div>
   )
 }
